f1b111719c269f4bcfa0986e450000f806edc7c4
abrim24 <abrim24@yahoo.com>
2015-11-17 20:21:13 -0700

    corrTrader web page

--- yahootest.py
+++ yahootest.py
@@ -4,6 +4,8 @@ import pdb
 import numpy as np
 import yahoo_finance as y
 from pandas import *
+import matplotlib.pyplot as plot
+
 
 
 def main():
@@ -24,13 +26,15 @@ def main():
 
     sectors = [it,energy,financials,industrials,healthcare,consumerdisc,consumerstaples]
 
+
     print energy
 
     print yahoo.get_price()
     print yahoo.get_open()
     print yahoo.get_prev_close()
-
-    histdata = yahoo.get_historical('2015-10-01','2015-10-12')
+    startDate = '2015-08-01'
+    endDate = '2015-10-29'
+    histdata = yahoo.get_historical(startDate,endDate)
     tickPrices = []
     for i in histdata: tickPrices.append(float(i['Adj_Close']))
     print histdata
@@ -43,7 +47,7 @@ def main():
         print "Sector: " + str(sectorsTxt[i])
         prices=[]
         for ticker in sector:
-            tickerPrices = y.Share(ticker).get_historical('2015-09-01','2015-10-13')
+            tickerPrices = y.Share(ticker).get_historical(startDate,endDate)
             cleanPrices =[]
             for j in tickerPrices: cleanPrices.append(float(j['Adj_Close']))
             prices.append(cleanPrices)
@@ -79,6 +83,84 @@ def main():
     print highcorrs
     print highpairs
 
-    print np.corrcoef(sectorPrices[0][1],sectorPrices[0][4])[1,0]
+    #printing all the highest correlations
+    i=0
+    plt=plot
+    for sec in sectors:
+        pairs=highpairs[i].split(',')
+        plt.plot(sectorPrices[i][sec.index(pairs[0])], label=str(pairs[0]), linewidth=5.0)
+        plt.plot(sectorPrices[i][sec.index(pairs[1])], label=str(pairs[1]), linewidth=5.0)
+        plt.xlabel(str(pairs[0] + " " + pairs[1] + " Correlation: " + str(highcorrs[i])))
+        plt.legend()
+        plt.show()
+        i+=1
+
+    newStocks=[]
+    newStocks2=[]
+    for stock in highpairs:
+        pair = stock.split(',')
+        newStocks.append(pair[0])
+        newStocks2.append(pair[1])
+
+    #recalculating correlations with highest correlating stocks
+    print "newStocks: ", newStocks
+    sectors = []
+    sectorsTxt = ['Pair1','Pair2']
+    sectors.append(newStocks)
+    sectors.append(newStocks2)
+    sectorPrices = []
+    i=0
+    for sector in sectors:
+        print "Sector: " + str(sectorsTxt[i])
+        prices=[]
+        for ticker in sector:
+            tickerPrices = y.Share(ticker).get_historical(startDate,endDate)
+            cleanPrices =[]
+            for j in tickerPrices: cleanPrices.append(float(j['Adj_Close']))
+            prices.append(cleanPrices)
+        sectorPrices.append(prices)
+        i+=1
+
+    highcorrs = []
+    highpairs = []
+    a=0
+    for i in sectorPrices:
+        b=0
+        highcorr=0.0
+        highpair=''
+        for j in sectorPrices[a]:
+            c=0
+            for k in sectorPrices[a]:
+                if b!=c:
+                    corr = np.corrcoef(sectorPrices[a][b],sectorPrices[a][c])[1,0]
+                    if corr > highcorr:
+                        highcorr= corr
+                        highpair= str(sectors[a][b] + ',' + sectors[a][c])
+                        print "corr: " , corr
+                        print "highcorr: " , highcorr
+                c+=1
+            b+=1
+        a+=1
+        highcorrs.append(highcorr)
+        highpairs.append(highpair)
+
+    print sectorPrices
+
+    #print np.corrcoef(sectorPrices[0],sectorPrices[1])[1,0]
+    print highcorrs
+    print highpairs
+
+
+    #printing all the highest correlations
+    i=0
+    plt=plot
+    for sec in sectors:
+        pairs=highpairs[i].split(',')
+        plt.plot(sectorPrices[i][sec.index(pairs[0])], label=str(pairs[0]), linewidth=5.0)
+        plt.plot(sectorPrices[i][sec.index(pairs[1])], label=str(pairs[1]), linewidth=5.0)
+        plt.xlabel(str(pairs[0] + " " + pairs[1] + " Correlation: " + str(highcorrs[i])))
+        plt.legend()
+        plt.show()
+        i+=1
 
 if __name__ == "__main__": main()
